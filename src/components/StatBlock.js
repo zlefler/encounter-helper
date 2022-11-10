91207ed9a7976197360077cfe424b0b950133e2f
Zach Lefler <zachary.lefler@gmail.com>
2022-11-10 09:11:41 +0900

    add abilities block, getting error when creating useState constants

--- src/components/StatBlock.js
+++ src/components/StatBlock.js
@@ -1,4 +1,5 @@
-import { Typography, Input } from '@mui/material';
+import { Typography, TextField } from '@mui/material';
+import useState from 'react';
 
 // con/str/dex/wis/cha/int
 // and those would propagate bonuses out to the other stuff
@@ -6,10 +7,24 @@ import { Typography, Input } from '@mui/material';
 //  all three saves, all the skills
 
 function StatBlock({ name }) {
+  //   const [score, setScore] = useState('');
+  //   const [mod, setMod] = useState('');
+
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
       <Typography>{name}:</Typography>
-      <Input variant="outlined">{name}</Input>
+      <TextField
+        // onChange={(e) => setScore(e.target.value)}
+        label="Score"
+        sx={{ width: '100px', margin: '5px' }}
+        // defaultValue={score}
+      />
+      <TextField
+        // onChange={(e) => setMod(e.target.value)}
+        label="Mod"
+        sx={{ width: '100px' }}
+        // defaultValue={mod}
+      />
     </div>
   );
 }
