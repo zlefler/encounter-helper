21307da5e321838f07749eb4b327d911ac3f1087
Zach Lefler <zachary.lefler@gmail.com>
2022-11-12 08:11:19 +0900

    add skills component and actual skills

---
+++ src/components/Skills.js
@@ -0,0 +1,41 @@
+function Skills() {
+  // acrobatics
+  // appraise
+  // bluff
+  // climb
+  // craft ___
+  // diplomacy
+  // disable device*
+  // disguise
+  // escape artist
+  // fly
+  // handle animal*
+  // heal
+  // intimidate
+  // knowledge (arcana)*
+  // knowledge (dungeoneering)*
+  // knowledge (engineering)*
+  // knowledge (geography)*
+  // knowledge (history)*
+  // knowledge (local)*
+  // knowledge (nature)*
+  // knowledge (nobility)*
+  // knowledge (planes)*
+  // knowledge (religion)*
+  // linguistics*
+  // perception
+  // perform ___
+  // profession ___*
+  // ride
+  // sense motive
+  // sleight of hand*
+  // spellcraft
+  // stealth
+  // survival
+  // swim
+  // use magic device*
+
+  return;
+}
+
+export default Skills;
