242312721fd0e0dcc3385b244129be433e87dd2f
Zach Lefler <zachary.lefler@gmail.com>
2022-11-15 08:54:38 +0900

    fix counter

--- src/components/Weapons.js
+++ src/components/Weapons.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 function Weapons() {
   const [weaponCount, setWeaponCount] = useState(1);
+  console.log(weaponCount);
 
   return (
     <div>
@@ -13,7 +14,11 @@ function Weapons() {
       {weaponCount > 2 && <Weapon />}
       {weaponCount > 3 && <Weapon />}
       {weaponCount < 4 && (
-        <Button onClick={(weaponCount) => setWeaponCount(weaponCount++)}>
+        <Button
+          onClick={() => {
+            setWeaponCount(weaponCount + 1);
+          }}
+        >
           Add Weapon
         </Button>
       )}
