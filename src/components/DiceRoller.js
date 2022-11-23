bf9a228cbc983164d8473ad735902f2074ee1fcf
Zach Lefler <zachary.lefler@gmail.com>
2022-11-22 23:44:54 -0800

    add state to diceroller

--- src/components/DiceRoller.js
+++ src/components/DiceRoller.js
@@ -1,6 +1,14 @@
+import { useState } from 'react';
+
 import { Card, Typography, Autocomplete } from '@mui/material';
 
 function DiceRoller() {
+  const [d4, setd4] = useState('0');
+  const [d6, setd6] = useState('0');
+  const [d8, setd8] = useState('0');
+  const [d10, set10] = useState('0');
+  const [d20, setd10] = useState('0');
+  const [dPct, setdPct] = useState('0');
   return (
     <Card>
       <Autocomplete label="D4" />
