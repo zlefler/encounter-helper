d99d95ba3d1d41e0369495107e86b20fd75ef697
Zach Lefler <zachary.lefler@gmail.com>
2022-11-23 00:01:01 -0800

    add labels for diceroller

--- src/components/DiceRoller.js
+++ src/components/DiceRoller.js
@@ -1,22 +1,45 @@
 import { useState } from 'react';
 
-import { Card, Typography, Autocomplete } from '@mui/material';
+import { Card, Typography, Autocomplete, Button } from '@mui/material';
 
 function DiceRoller() {
   const [d4, setd4] = useState('0');
   const [d6, setd6] = useState('0');
   const [d8, setd8] = useState('0');
-  const [d10, set10] = useState('0');
-  const [d20, setd10] = useState('0');
-  const [dPct, setdPct] = useState('0');
+  const [d10, setd10] = useState('0');
+  const [d20, setd20] = useState('0');
+  const [dPct, setDPct] = useState('0');
+
+  const dieRolls = ['1', '2', '3', '4', '5'];
   return (
     <Card>
-      <Autocomplete label="D4" />
-      <Autocomplete label="D6" />
-      <Autocomplete label="D8" />
-      <Autocomplete label="D10" />
-      <Autocomplete label="D20" />
+      <Autocomplete
+        onClick={(e) => setd4(e.target.value)}
+        options={dieRolls}
+        label="D4"
+      />
+      <Autocomplete
+        onClick={(e) => setd6(e.target.value)}
+        options={dieRolls}
+        label="D6"
+      />
+      <Autocomplete
+        onClick={(e) => setd8(e.target.value)}
+        options={dieRolls}
+        label="D8"
+      />
+      <Autocomplete
+        onClick={(e) => setd10(e.target.value)}
+        options={dieRolls}
+        label="D10"
+      />
+      <Autocomplete
+        onClick={(e) => setd20(e.target.value)}
+        options={dieRolls}
+        label="D20"
+      />
       <Autocomplete label="D%" />
+      <Button>Roll</Button>
     </Card>
   );
 }
