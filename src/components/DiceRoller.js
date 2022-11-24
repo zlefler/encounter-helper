7fb56ca29e931ceaf028db1e5e0310fb417bc437
Zach Lefler <zachary.lefler@gmail.com>
2022-11-23 17:20:36 -0800

    finish diceroller basic functionality

--- src/components/DiceRoller.js
+++ src/components/DiceRoller.js
@@ -1,67 +1,94 @@
 import { useState } from 'react';
 
-import {
-  Card,
-  Typography,
-  Autocomplete,
-  Button,
-  TextField,
-} from '@mui/material';
+import { Card, Button, TextField, Typography } from '@mui/material';
 
 function DiceRoller() {
-  const [d4, setd4] = useState('0');
-  const [d6, setd6] = useState('0');
-  const [d8, setd8] = useState('0');
-  const [d10, setd10] = useState('0');
-  const [d20, setd20] = useState('0');
-  // const [dPct, setDPct] = useState('0');
+  const [d4, setd4] = useState('');
+  const [d6, setd6] = useState('');
+  const [d8, setd8] = useState('');
+  const [d10, setd10] = useState('');
+  const [d20, setd20] = useState('');
+  const [dpct, setdpct] = useState('');
+  const [rollTotal, setRollTotal] = useState('0');
 
-  const dieRolls = ['0', '1', '2', '3', '4', '5'];
+  function rollDice() {
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * max) + 1;
+    }
+
+    let total = 0;
+    for (let i = 0; i < d4; i++) {
+      total += getRandomInt(4);
+    }
+    for (let i = 0; i < d6; i++) {
+      total += getRandomInt(6);
+    }
+    for (let i = 0; i < d8; i++) {
+      total += getRandomInt(8);
+    }
+    for (let i = 0; i < d10; i++) {
+      total += getRandomInt(10);
+    }
+    for (let i = 0; i < d20; i++) {
+      total += getRandomInt(20);
+    }
+    for (let i = 0; i < dpct; i++) {
+      total += getRandomInt(100);
+    }
+
+    setRollTotal(total);
+  }
 
   function clearRolls() {
-    setd4('0');
-    setd6('0');
-    setd8('0');
-    setd10('0');
-    setd20('0');
+    setd4('');
+    setd6('');
+    setd8('');
+    setd10('');
+    setd20('');
+    setdpct('');
   }
 
   return (
     <Card>
-      <Autocomplete
-        onChange={(e) => setd4(e.target.value)}
-        options={dieRolls}
+      <TextField
         sx={{ width: '80px', margin: '6px' }}
         label="D4"
+        onChange={(e) => setd4(e.target.value)}
         value={d4}
       />
-      <Autocomplete
+      <TextField
+        sx={{ width: '80px', margin: '6px' }}
+        label="D6"
         onChange={(e) => setd6(e.target.value)}
-        options={dieRolls}
-        sx={{ width: '80px', margin: '6px' }}
-        renderInput={(params) => <TextField {...params} label="D6" />}
+        value={d6}
       />
-      <Autocomplete
+      <TextField
+        sx={{ width: '80px', margin: '6px' }}
+        label="D8"
         onChange={(e) => setd8(e.target.value)}
-        options={dieRolls}
-        sx={{ width: '80px', margin: '6px' }}
-        renderInput={(params) => <TextField {...params} label="D8" />}
+        value={d8}
       />
-      <Autocomplete
+      <TextField
+        sx={{ width: '80px', margin: '6px' }}
+        label="D10"
         onChange={(e) => setd10(e.target.value)}
-        options={dieRolls}
-        sx={{ width: '80px', margin: '6px' }}
-        renderInput={(params) => <TextField {...params} label="D10" />}
+        value={d10}
       />
-      <Autocomplete
+      <TextField
+        sx={{ width: '80px', margin: '6px' }}
+        label="D20"
         onChange={(e) => setd20(e.target.value)}
-        options={dieRolls}
-        sx={{ width: '80px', margin: '6px' }}
-        renderInput={(params) => <TextField {...params} label="D20" />}
+        value={d20}
       />
-      {/* <Autocomplete label="D%" /> */}
-      <Button>Roll</Button>
+      <TextField
+        sx={{ width: '80px', margin: '6px' }}
+        label="D%"
+        onChange={(e) => setdpct(e.target.value)}
+        value={dpct}
+      />
+      <Button onClick={() => rollDice()}>Roll</Button>
       <Button onClick={() => clearRolls()}>Clear</Button>
+      {rollTotal && <Typography>Total: {rollTotal}</Typography>}
     </Card>
   );
 }
