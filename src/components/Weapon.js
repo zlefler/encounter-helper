dcf7f83eba4514c2de75d259a7e2bcdbc1a80c47
Zach Lefler <zachary.lefler@gmail.com>
2022-11-27 12:50:42 -0800

    add autocompletes for crit range/multiplier

--- src/components/Weapon.js
+++ src/components/Weapon.js
@@ -1,14 +1,17 @@
-import { TextField, Card } from '@mui/material';
+import { TextField, Autocomplete, Card } from '@mui/material';
 
 function Weapon() {
+  const critRangeOptions = ['20', '19-20', '18-20', 'None'];
+  const critDamageOptions = ['x2', 'x3', 'None'];
   return (
     <Card variant="contained">
       <TextField sx={{ width: '250px', margin: '2px' }} label="Name" />
       <TextField sx={{ width: '100px', margin: '2px' }} label="Attack Bonus" />
+      <Autocomplete options={critRangeOptions} label="Crit Range" />
+      <Autocomplete options={critDamageOptions} label="Multiplier" />
       <TextField sx={{ width: '100px', margin: '2px' }} label="Crit Range" />
       <TextField sx={{ width: '100px', margin: '2px' }} label="Type" />
       <TextField sx={{ width: '100px', margin: '2px' }} label="Range" />
-      <TextField sx={{ width: '150px', margin: '2px' }} label="Damage" />
     </Card>
   );
 }
