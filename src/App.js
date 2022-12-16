22c25c153d0d3b6cbb6721b458afc5dccbe1f199
Zach Lefler <zachary.lefler@gmail.com>
2022-12-16 12:01:51 -0800

    Revert "add rails api folders to project"
    This reverts commit e58d39c3bff694ad257b112d252613258efbd5df.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import { BrowserRouter as Router, Switch, useLocation } from 'react-router-dom';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
