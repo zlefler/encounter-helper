c9641f73eb38f2cbddbefe44e58f04303f4aed8e
Zach Lefler <zachary.lefler@gmail.com>
2022-12-28 15:12:19 -0800

    add skills#create and skills#show

--- app/controllers/abilities_controller.rb
+++ app/controllers/abilities_controller.rb
@@ -2,11 +2,7 @@ class AbilitiesController < ApplicationController
 
     def create
         character = Character.find(params[:characterID])
-        # if character.abilities
-        #     ability = character.abilities.first.update(strength: params[:strength], dexterity: params[:dexterity, constitution: params[:constitution], intelligence: params[:intelligence], wisdom: params[:wisdom], charisma: params[:charisma], character_id: ability.character_id])
-        # else
-            ability = Ability.create!(strength: params[:strength], dexterity: params[:dexterity], constitution: params[:constitution], intelligence: params[:intelligence], wisdom: params[:wisdom], charisma: params[:charisma], character_id: params[:characterID])
-        # end
+        ability = Ability.create!(strength: params[:strength], dexterity: params[:dexterity], constitution: params[:constitution], intelligence: params[:intelligence], wisdom: params[:wisdom], charisma: params[:charisma], character_id: params[:characterID])
         render json: ability, status: :created
     rescue ActiveRecord::RecordInvalid => invalid
         render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
