0ea393def07aa5d4c20bbad6a6867552d3886716
Zach Lefler <zachary.lefler@gmail.com>
2022-12-29 13:09:24 -0800

    change seeds to work with new ability/skill setup

--- client/src/App.js
+++ client/src/App.js
@@ -8,17 +8,7 @@ import CharacterPicker from './components/CharacterPicker';
 import Navbar from './components/Navbar';
 import Name from './components/Name';
 
-// TODO: Check if new ability update works
-
 // TODO: Make data persist when building character on front end
-
-// TODO: build functions to save/get stats from character on back end
-
-// TODO: build "add new character" button
-
-// TODO: Figure out where current character ID should live. Does it live in state
-// and gets passed on every fetch? Probably. If so, make sure back end
-// knows where to find it.
 
 // TODO: Refactor Skills like you did Abilities
 
