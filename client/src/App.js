86b7e0ea57faa9a9a141997f87d933d463b0f13c
Zach Lefler <zachary.lefler@gmail.com>
2022-12-29 13:06:58 -0800

    remove comments

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import CharacterPicker from './components/CharacterPicker';
 import Navbar from './components/Navbar';
 import Name from './components/Name';
 
-// TODO: Check if new ability update works.
+// TODO: Check if new ability update works
 
 // TODO: Make data persist when building character on front end
 
